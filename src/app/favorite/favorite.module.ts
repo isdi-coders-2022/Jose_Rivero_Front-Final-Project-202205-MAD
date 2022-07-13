235d1601b7570583aebc941248f040b63405044a
JoseRivero <josearmandorr95@gmail.com>
2022-07-13 17:29:40 +0200

    Add services & test

--- src/app/favorite/favorite.module.ts
+++ src/app/favorite/favorite.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavoriteComponent } from './favorite.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FavoriteComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    LoginFormComponent,
-    RegisterFormComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginFormComponent, RegisterFormComponent, LoginComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class LoginModule { }
+export class LoginModule {}
