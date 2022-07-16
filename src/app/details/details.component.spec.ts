2e884c3cfbb97754c0c4934294a7bf2180c1cd6c
JoseRivero <josearmandorr95@gmail.com>
2022-07-16 19:41:07 +0200

    update pages and fix tests

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -29,7 +30,7 @@ describe('DetailsComponent', () => {
         },
         provideMockStore({ initialState: mockInitialState }),
       ],
-      imports: [RouterTestingModule, CoreModule, CommonModule],
+      imports: [RouterTestingModule, CoreModule, CommonModule, FormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DetailsComponent);
@@ -39,9 +40,19 @@ describe('DetailsComponent', () => {
   });
 
   it('should create', () => {
+    component.saveHandle();
     expect(component).toBeTruthy();
   });
-  /*  describe('When', () => {
-    it('should first', () => {});
-  }); */
+  describe('When call the function with increase', () => {
+    it('should first', () => {
+      component.handleSubmit('increase');
+      expect(component.quantity).toBe(2);
+    });
+  });
+  describe('When call the function with decrease', () => {
+    it('should first', () => {
+      component.handleSubmit('decrease');
+      expect(component.quantity).toBe(0);
+    });
+  });
 });
--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -4,9 +4,16 @@ import { DetailsComponent } from './details.component';
 import { CoreModule } from '../core/core.module';
 import { DetailsRoutingModule } from './details-routing-module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DetailsComponent],
-  imports: [CommonModule, CoreModule, DetailsRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    CoreModule,
+    DetailsRoutingModule,
+    RouterModule,
+    FormsModule,
+  ],
 })
 export class DetailsModule {}
